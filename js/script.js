2f50ae4d749c17dee01c7640ab535c9e2efa6631
michele <micheleepedrotti@gmail.com>
2021-03-24 15:54:35 +0100

    part 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+function show(obj){
+
+    for (var key in obj){
+
+        console.log(obj[key]);
+    };
+    
+
+};
+
+
 function init(){
 
+    var students = {
+        'name' : 'Tizio',
+        'lastname': 'Dei Tizi',
+        'age' : 30
+    };
+
+    show(students)
 };
 
 $('document').ready(init)
